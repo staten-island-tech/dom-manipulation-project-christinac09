debaf629f25450c809b4b066149a21ba4537217e
christina <christinac272@nycstudents.net>
2024-10-06 22:25:37 -0400

    idkkkk

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const DOMSelectors = {
   header: document.querySelector("h1"),
-  button: document.querySelector(".btn"),
+  removeButton: document.querySelector(".remove-btn"),
   form: document.querySelector(".form"),
+  nameInput: document.querySelector("#name-input"),
+  descInput: document.querySelector("#desc-input"),
+  imgInput: document.querySelector("#img-input")
 };
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault(); // default is refresh the page
-  let name = document.querySelector(".").value;
-  console.log(name);
+  let name = DOMSelectors.nameInput.value;
+  let desc = DOMSelectors.descInput.value;
+  let img = DOMSelectors.imgInput.value;
+  let newAlbum = {
+    name: name,
+    description: desc,
+    imageLink: img
+  }
+  console.log(newAlbum)
 });
