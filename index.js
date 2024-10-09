05c283ffaa85b713381c0de9f8011f933d420c75
christina <158211764+christinac09@users.noreply.github.com>
2024-10-09 13:57:45 -0400

    fdas

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const DOMSelectors = {
   nameInput: document.querySelector("#name-input"),
   artistInput: document.querySelector("#artist-input"),
   imgInput: document.querySelector("#img-input"),
+  container: document.querySelector(".container"),
 };
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault(); // default is refresh the page
-  const newAlbum = {
-    name: DOMSelectors.nameInput.value,
-    artist: DOMSelectors.artistInput.value,
-    imageLink: DOMSelectors.imgInput.value,
-  };
-  console.log(newAlbum);
-});
+
+function getInput() {
+  DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault(); // default is refresh the page
+    const newAlbum = {
+      name: DOMSelectors.nameInput.value,
+      artist: DOMSelectors.artistInput.value,
+      imageLink: DOMSelectors.imgInput.value,
+    };
+    return newAlbum;
+  });
+}
+const a = getInput();
+console.log(a);
+function createCard(album) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card"><h2 class="card-header">${album.name}</h2><button class="remove-btn">remove</button></div>`
+  );
+}
+
+createCard(a);
