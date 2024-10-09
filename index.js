195d618f5aebcceeb21faa2878e663eba31ff6a7
christina <158211764+christinac09@users.noreply.github.com>
2024-10-09 13:19:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const DOMSelectors = {
   form: document.querySelector(".form"),
   nameInput: document.querySelector("#name-input"),
   descInput: document.querySelector("#desc-input"),
-  imgInput: document.querySelector("#img-input")
+  imgInput: document.querySelector("#img-input"),
 };
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault(); // default is refresh the page
-  let name = DOMSelectors.nameInput.value;
-  let desc = DOMSelectors.descInput.value;
-  let img = DOMSelectors.imgInput.value;
-  let newAlbum = {
-    name: name,
-    description: desc,
-    imageLink: img
-  }
-  console.log(newAlbum)
+  const newAlbum = {
+    name: DOMSelectors.nameInput.value,
+    description: DOMSelectors.descInput.value,
+    imageLink: DOMSelectors.imgInput.value,
+  };
+  console.log(newAlbum);
 });
