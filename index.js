c4b298ec1d12c22dbfb91068ae495823957fbb7b
christina <christinac272@nycstudents.net>
2024-10-12 00:10:50 -0400

    fasd

--- index.js
+++ index.js
@@ -15,30 +15,57 @@ function createCard() {
     artist: DOMSelectors.artistInput.value,
     imageLink: DOMSelectors.imgInput.value,
   };
+  // check if all input fields are filled out
+  if (!checkIfFilled(newAlbum)) {
+    return
+  }
+  console.log(newAlbum);
+  removeInput();
+  injectCard(newAlbum);
+  removeCard();
+};
+
+function injectCard(card) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
-      <h2 class="card-name">${newAlbum.name}</h2>
-      <h3 class="card-artist">${newAlbum.artist}</h3>
-      <img src="${newAlbum.imageLink}" alt="" class="card-img">
+      <h2 class="card-name">${card.name}</h2>
+      <h3 class="card-artist">${card.artist}</h3>
+      <img src="${card.imageLink}" alt="" class="card-img">
       <button class="card-removeBtn">Remove</button>
     </div>`
   );
   DOMSelectors.removeButtons = document.querySelectorAll(".card-removeBtn"); // so that it includes the button of the new card
-  DOMSelectors.allInputs.forEach((input) => (input.value = ""));
-}
+};
 
 function removeCard() {
-  DOMSelectors.removeButtons.forEach((button) =>
-    button.addEventListener("click", function (event) {
+  DOMSelectors.removeButtons.forEach((btn) =>
+    btn.addEventListener("click", function (event) {
       event.preventDefault();
       event.target.parentElement.remove();
     })
   );
+};
+
+function removeInput() {
+  DOMSelectors.allInputs.forEach((input) => (input.value = ""));
+}
+
+function checkIfFilled(object) {
+  const inputs = Object.values(object);
+  let counter=0;
+  inputs.forEach((value) => {if (value==="") {
+    counter += 1
+  }});
+  if (counter>0) {
+    alert("Please fill out ALL input fields")
+    return false
+  } else {
+    return true
+  }
 }
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   createCard();
-  removeCard();
 });
